f98b651c2779e03abf6d66b627dbb05a8cd25f42
Mattheonpil <mpellet@net-c.fr>
2025-06-06 10:04:27 +0200

    travail sur navbar

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,8 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+const navbar = document.querySelector('.main-nav');
+const mainContent = document.querySelector('.main-content');
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+mainContent.style.padding = navbar.offsetHeight+'px';
+
